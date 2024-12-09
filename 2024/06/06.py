e41b6761bb7a7166dc7b3fa801dd7dd792ee21ea
Nicolas Meier <19802619+nicolasmeier@users.noreply.github.com>
2024-12-09 16:25:32 +0100

    6

--- 2024/06/06.py
+++ 2024/06/06.py
@@ -10,15 +10,14 @@ os.chdir(os.path.dirname(sys.argv[0]))
 
 def trans(x:str):
     x = x.replace("^","G")
-    return x
+    return list(x)
     
 li = []
 with open("06.txt") as f:
     li = [trans(x.strip()) for x in f.readlines()]
 
 
-grid = np.array(li)
-[print(l) for l in grid]
+grid = li
 
 direction = {
     0: (-1,0),
@@ -32,26 +31,28 @@ cd = 0
 gx = 0
 gy = 0
 part2 = 0
-grid = np.array(li)
+grid = li
 v = set()
 for x,r in enumerate(li):
     if "G" in r:
         gx = x
         gy = r.index("G")
 
-print((gx,gy),li[gx][gy])
 v.add((gx,gy))
+ogx = gx
+ogy = gy
 
-def checkLoop(ld,lx,ly,p):
+def checkLoop(ld,lx,ly):
     loop = set()
     loop.add((ld,lx,ly))
-    ld = (ld + 1) % 4
+    #ld = (ld + 1) % 4
     while(0 <= lx+direction[ld][0] < mx and 0 <= ly+direction[ld][1] < my):
         if grid[lx+direction[ld][0]][ly+direction[ld][1]] == '#':
             ld = (ld + 1) % 4
         else:
             lx = lx+direction[ld][0]
             ly = ly+direction[ld][1]
+
         if (ld,lx,ly) in loop:
             return True
         loop.add((ld,lx,ly))
@@ -65,13 +66,21 @@ while(0 <= gx+direction[cd][0] < mx and 0 <= gy+direction[cd][1] < my):
     if grid[gx+direction[cd][0]][gy+direction[cd][1]] == '#':
         cd = (cd + 1) % 4
     else:
-        
-        if (gx+direction[cd][0],gy) not in v and checkLoop(cd,gx,gy,p):
-            barriar.add((gx+direction[cd][0],gy+direction[cd][1]))
-
         gx = gx+direction[cd][0]
         gy = gy+direction[cd][1]
         v.add((gx,gy))
+
+
+
+for x,y in v:
+    grid[x][y] = '#'
+    if checkLoop(0,ogx,ogy):
+        barriar.add((x,y))
+    
+    grid[x][y] = '.'
+    grid[ogx][ogy] = 'G'
+
+
 
 print(len(v))
 print(len(barriar))
